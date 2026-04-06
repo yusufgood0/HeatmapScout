102e309770e5910df02221eaf44aa75365fb17e7
Yusuf amin <yusufgood0@gmail.com>
2026-04-06 14:59:33 -0400

    now caches data for offline

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,10 @@ export default defineConfig({
           { src: 'icons/icon-512.png', sizes: '512x512', type: 'image/png' },
         ],
       },
-    }),
+      workbox: {
+        // Cache everything inside dist
+        globPatterns: ['**/*.{js,css,html,png,json,svg}'],
+      },
+    })
   ],
 });
