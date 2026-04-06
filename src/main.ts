3f665ed2dcb4c6f8e9b42887233c77aa5d5151ae
Yusuf amin <yusufgood0@gmail.com>
2026-04-06 01:17:17 -0400

    added API injecter to to yaml

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-// disabled as it confuses github's secret scanning. Just remember to create a .env file with API_KEY and SHEET_ID defined when running this locally.
-// import dotenv from "dotenv";
-// dotenv.config(); // loads variables from .env into process.env
+/*
+ disabled as it confuses github's secret scanning. Just remember to create a .env file with API_KEY and SHEET_ID defined when running this locally.
+ import dotenv from "dotenv";
+ dotenv.config(); // loads variables from .env into process.env
+*/
 
-const API_KEY = process.env.API_KEY;
-const SHEET_ID = process.env.SHEET_ID;
+// or, if using plain TS with a custom build script:
+export const API_KEY = process.env.API_KEY;
+export const SHEET_ID = process.env.SHEET_ID;
 
 if (!API_KEY) { throw new Error("API_KEY is not defined in .env");}
 if (!SHEET_ID) { throw new Error("SHEET_ID is not defined in .env");}
