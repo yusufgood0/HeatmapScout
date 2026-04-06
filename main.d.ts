f9c826c6bef6f615b51df25b80566ce711628560
Yusuf amin <yusufgood0@gmail.com>
2026-04-06 00:22:39 -0400

    Updates

---
+++ main.d.ts
@@ -0,0 +1,39 @@
+interface SheetRequest {
+    range?: string | undefined;
+    APIkey?: string | undefined;
+    sheetID?: string | undefined;
+    sheetName?: string | undefined;
+    filter?: Filter | undefined;
+}
+interface Filter {
+    key: string;
+    value: string | number;
+}
+export interface VertexPoint {
+    x: number;
+    y: number;
+}
+export declare function GetTranslations(): any;
+export declare function DecodePolyline(str: string): VertexPoint[];
+export declare function CompileAndAverage(records: Record<string, string>[]): Record<string, string>;
+export declare function FetchSheetData(requestInput: SheetRequest | null): Promise<Record<string, string>[] | null>;
+export declare function ClearCanvas(): void;
+export declare function DrawPaths(paths: VertexPoint[][], pathData: PathData | null): void;
+interface PathData {
+    color1: {
+        r: number;
+        g: number;
+        b: number;
+    } | string;
+    color2: {
+        r: number;
+        g: number;
+        b: number;
+    } | string;
+    opacity: number;
+}
+export declare function ImportAndDrawPath(teamNumber?: number, pathData?: PathData | null): void;
+export declare function SetDisplayValue(title: string, value: string): void;
+export declare function GetDisplayData(): Record<string, string>;
+export {};
+//# sourceMappingURL=main.d.ts.map
